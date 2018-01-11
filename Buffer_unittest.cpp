553fcf10d2e229b8ebcdb6ab76e558b118dd96b2
Norah <wjing931202@163.com>
2018-01-11 18:41:48 +0800

    add Buffer_unittest.cpp

---
+++ Buffer_unittest.cpp
@@ -0,0 +1,15 @@
+#include"Buffer.h"
+#include<cassert>
+
+
+int main()
+{
+	testBufferAppendRetrieve();
+}
+
+void testBufferAppendRetrieve()
+{
+	Buffer buf;
+	assert(buf.readableSize()==0);
+	assert(buf.writableSize()==)
+}
